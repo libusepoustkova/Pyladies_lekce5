13ae5caefca9e250394c8c20a6b10ac184cba55e
libusepoustkova <32682660+libusepoustkova@users.noreply.github.com>
2019-10-14 20:35:57 +0200

    Lekce 5 - procvicovani a soubory
    soubory k lekci 5

---
+++ funkce_na_retezce_ukol.py
@@ -0,0 +1,36 @@
+# Napis funkci, ktera prevede text na velka pismena.
+def upper_char(retezec):
+    pass
+
+
+# Napis funkci, ktera pocita pocet znaku v retezci.
+def count_char(retezec):
+    pass
+
+
+# Napis funcki, ktera pocita pocet mezer v retezci
+def count_spaces(retezec):
+    pass
+
+
+# Napis funcki, ktera pocita pocet "samohlasek"
+def count_aeiou(retezec):
+    pass
+
+# Napis funkci, ktera vlozi retezec doprostred jineho retezce.
+def string_in_string(ret, retvloz):
+    pass
+
+
+# Napis funkci, ktera obrati retezec
+def reverse_string(retezec):
+    pass
+
+#
+retezec_znaku = 'pokus'
+print(reverse_string(retezec_znaku))
+print(upper_char(retezec_znaku))
+print(count_char(retezec_znaku))
+print(count_spaces(retezec_znaku))
+print(count_aeiou(retezec_znaku))
+print(string_in_string(retezec_znaku, 'ahoj'))
---
+++ soubory_ukoly_na_procviceni.py
@@ -0,0 +1,26 @@
+"""
+samostudium:
+https://naucse.python.cz/2019/plzen-podzim-2019/beginners/files/
+
+Ukoly:
+
+Otevřete jeden soubor pro čtení, druhý pro zápis.
+
+1.Do druhého souboru přendejte obsah prvního souboru - samozřejmě programem v pythonu
+
+2.Do druhého souboru přendejte obsah prvního souboru ale všechna písmena převeďte na velká
+
+3.Do druhého souboru přendejte každou sudou řádku.
+
+4.Do druhého souboru napište, kolik znaků má každá řádka.
+
+5.Do druhého souboru napište kolik nebílých znaků má každá řádka
+
+6.Do druhého souboru napiště počet samohlásek v textu každé řádky
+
+7.Kolik odstavců soubor obsahuje?
+
+samostudium:
+https://naucse.python.cz/2019/brno-podzim-pondeli/intro/pathlib/
+"""
+
